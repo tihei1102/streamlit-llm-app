845dec9bd0c683a7f839c030323d0d4de82d0b3d
sato <tihei1102@gmail.com>
2025-03-03 19:40:41 +0900

    first commit.

---
+++ app.py
@@ -0,0 +1,62 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+
+st.title("課題アプリ: 専門家チャットボット")
+
+st.write("##### 動作モード1: リサーチ手法専門家")
+st.write("リサーチ手法関連を聞くことで、専門家としてアドバイスしてくれます")
+st.write("##### 動作モード2: 分析手法専門家")
+st.write("分析手法関連を聞くことで、専門家としてアドバイスしてくれます")
+
+selected_item = st.radio(
+    "動作モードを選択してください。",
+    ["リサーチ手法専門家", "分析手法専門家"]
+)
+
+st.divider()
+
+if selected_item == "リサーチ手法専門家":
+    input_message = st.text_input(label="リサーチの専門家に聞きたいことを入力してください。")
+
+else:
+    input_message = st.text_input(label="分析手法の専門家に聞きたいことを入力してください。")
+
+if st.button("実行"):
+    st.divider()
+    import os
+    from google.colab import userdata
+    os.environ["OPENAI_API_KEY"] = userdata.get("OPENAI_API_KEY")
+
+    if selected_item == "リサーチ手法専門家":
+        if input_message:
+            from langchain_openai import ChatOpenAI
+            from langchain.schema import SystemMessage, HumanMessage, AIMessage
+            llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+
+            messages = [
+            SystemMessage(content="あなたはリサーチ手法の専門家です"),
+            HumanMessage(content=input_message),
+            ]
+            result = llm(messages)
+            st.write(result)
+
+        else:
+            st.error("対象となるテキストを入力してから「実行」ボタンを押してください。")
+
+    else:
+        if input_message:
+            from langchain_openai import ChatOpenAI
+            from langchain.schema import SystemMessage, HumanMessage, AIMessage
+            llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+
+            messages = [
+            SystemMessage(content="あなたは分析手法の専門家です"),
+            HumanMessage(content=input_message),
+            ]
+            result = llm(messages)
+            st.write(result)
+
+        else:
+            st.error("対象となるテキストを入力してから「実行」ボタンを押してください。")
